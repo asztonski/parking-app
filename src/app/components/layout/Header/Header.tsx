476d892d212e9c08071f29e96088467cc4159212
Dominik Supinski <dominik.supinski@getsavvy.com>
2023-11-24 15:44:08 +0000

    post-css library added + image optymalization

--- src/app/components/layout/Header/Header.tsx
+++ src/app/components/layout/Header/Header.tsx
@@ -5,8 +5,18 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header className="w-full flex justify-center p-8">
-      <Link href={'/'} className="max-h-96 w-full max-w-xl relative aspect-[2000_/_1247]">
-        <Image className="" alt="Image of CarPark logo brand" fill src={Logo} />
+      <Link
+        href={"/"}
+        className="max-h-96 w-full max-w-xl relative aspect-[2000_/_1247]"
+      >
+        <Image
+          className=""
+          alt="Image of CarPark logo brand"
+          fill
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          src={Logo}
+        />
       </Link>
     </header>
   );
