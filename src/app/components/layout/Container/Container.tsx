4eb895ee51d87decd46f13a263d898b090cb9e73
Dominik Supinski <dominik.supinski@getsavvy.com>
2023-11-24 12:54:38 +0000

    First test ready

--- src/app/components/layout/Container/Container.tsx
+++ src/app/components/layout/Container/Container.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 
 export const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="relative p-4 md:p-10 w-full flex flex-col justify-center gap-10 after:absolute after:-z-10 after:bg-secondary after:opacity-20 after:w-full after:h-full after:rounded-lg after:inset-0">
+    <div data-testid="container" className="relative p-4 md:p-10 w-full flex flex-col justify-center gap-10 after:absolute after:-z-10 after:bg-secondary after:opacity-20 after:w-full after:h-full after:rounded-lg after:inset-0">
       {children}
     </div>
   );
--- src/app/components/layout/Footer/Footer.tsx
+++ src/app/components/layout/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export const Footer = () => {
     return (
-        <footer className="bg-secondary flex p-4 justify-center text-xl gap-4">
+        <footer className="bg-secondary flex p-4 justify-center md:text-xl gap-4">
             <Link href={'/'}>CarPark 2023 |</Link>
             <Link href={'/terms'}>Terms & Conditions</Link>
         </footer>
